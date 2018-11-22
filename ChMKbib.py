2f46cedd1b93cf7a9f43d151f4e4ea6a133cf2b6
Yannick Daniel <yannick.daniel@siemens.com>
2018-11-22 14:02:27 +0100

    neue Datei:     .ChMKbib.py.swn 	neue Datei:     .livestatus.py.swp 	geändert:       ChMKbib.py 	neue Datei:     ChMKbib.pyc 	geändert:       ServiceLOG.txt 	geändert:       Textfile.txt 	neue Datei:     all_Commands.txt 	neue Datei:     livestatus.pyc 	geändert:       prototyp.py

--- ChMKbib.py
+++ ChMKbib.py
@@ -1,10 +1,10 @@
-#!/usr/bin/python
+#!/usr/bin/python2.7
 
 import os, sys, livestatus, time, calendar
 
 
 def log_Since_Time(socket_path):
-    import os, sys, livestatus, time, calendar
+    import  sys, livestatus, time, calendar
     t=calendar.timegm(time.gmtime())    
     zeitraum=input("Auf welchen Zeitraum sollen die Ergebnisse eingegrenzt werden? Eingabe in Minuten:")
     zeitraum=zeitraum*60
@@ -12,10 +12,10 @@ def log_Since_Time(socket_path):
     try:
         services = livestatus.SingleSiteConnection(socket_path).query_table("GET log\nFilter: time >= %d\nFilter: class = 1\nColumns: host_name service_description time state\nOutputFormat: python"%t)
         for host_name, service_description,time, state in services:
-            print "%-12s %-25s %-12s %-5s" % (host_name, service_description,time, state)
+            print ("%-12s %-25s %-12s %-5s" % (host_name, service_description,time, state))
 
-    except Exception, e:
-        print "Livestatus Error %s" % str(e)
+    except Exception as e:
+        print ("Livestatus Error %s" % str(e))
 
 ##########################################################################################
 def local_Connection():
@@ -31,31 +31,31 @@ def local_Connection():
 ##########################################################################################
 def show_Hosts(socket_path):
     try:
-        print "\nHosts:"
+        print ("\nHosts:")
         hosts = livestatus.SingleSiteConnection(socket_path).query_table("GET hosts\nColumns: name alias address")
         for name, alias, address in hosts:
-            print "%-16s %-16s %s" % (name, address, alias)
-    except Exception, e:
-        print "Livestatus error %s" % str(e)
+            print ("%-16s %-16s %s" % (name, address, alias))
+    except Exception as e:
+        print ("Livestatus error %s" % str(e))
 
 
 ##########################################################################################
 def show_Services(socket_path):
     try:
-        print "\nServices mit genauer Beschreibung"
+        print ("\nServices mit genauer Beschreibung")
         t = time.ctime()
-        print "Zeitpunkt der Abfrage: %s\n" % t
+        print ("Zeitpunkt der Abfrage: %s\n" % t)
         services=livestatus.SingleSiteConnection(socket_path).query_table("GET services\nColumns: host_name description state")
 
         for host_name, description, state in services:
-            print "%-10s %-20s %s" % (host_name, description, state)
+            print ("%-10s %-20s %s" % (host_name, description, state))
         fobj_out = open("/home/service/Schreibtisch/Testprogramme/ServiceLOG.txt","a")
         fobj_out.write("Zeitpunkt der Abfrage: %s\n" %t)
         for host_name in services:
             fobj_out.write("%s\n" %(host_name))
 
-    except Exception, e:
-        print "Livestatus Error %s" % str(e)
+    except Exception as e:
+        print ("Livestatus Error %s" % str(e))
     return services
 
 #########################################################################################
@@ -65,10 +65,10 @@ def show_New_Logs(socket_path):
        t = calendar.timegm(time.gmtime())
        logs = livestatus.SingleSiteConnection(socket_path).query_table("GET log\nColumns: host_name service_description time state\nWaitTrigger: log\nFilter: time >= %d\nFilter: class = 1\nOutputFormat: python"%t)
        if logs:
-           print logs, t
+           print (logs, t)
       
-    except Exception, e:
-        print "Livestatus Error %s" % str(e)
+    except Exception as e:
+        print ("Livestatus Error %s" % str(e))
 
     return logs
 
@@ -82,11 +82,6 @@ def write_To_Textfile(data):
 
 #########################################################################################
 def show_Logs_Test(socket_path):
-
-
-
-
-
     try:
        services=livestatus.SingleSiteConnection(socket_path).query_table("GET services\nColumns: host_name description state")
        t = calendar.timegm(time.gmtime())
@@ -94,10 +89,10 @@ def show_Logs_Test(socket_path):
            
            logs = livestatus.SingleSiteConnection(socket_path).query_table("GET log\nWaitTrigger: downtime\nWaitTimeout: 5000\nFilter: time >= %d\nColumns: host_name service_description time state" %(t))
        if logs:
-           print logs, t
+           print (logs, t)
            return logs
-    except Exception, e:
-        print "Livestatus Error %s" % str(e)
+    except Exception as e:
+        print ("Livestatus Error %s" % str(e))
 
 #    return logs
 
@@ -116,6 +111,43 @@ def show_Availability(socket_path):
        
        
         for line in availability:
-            print line
-    except Exception, e:
-        print "Livestatus Error %s" % str(e)
+            print(line)
+    except Exception as e:
+        print ("Livestatus Error %s" % str(e))
+
+###############################################################################
+
+def send_Command(socket_path):
+    import time
+
+    liste_anzeigen = raw_input("Wollen Sie eine Liste mit den verfuegbaren Befehlen angezeigt bekommen?(Ja/Nein) Eingabe: ")
+    proceed =['y', 'Y', 'Ja', 'ja'] 
+    read_obj = open("all_Commands.txt")
+    all_Commands = read_obj.read()
+
+    if liste_anzeigen in proceed: 
+       print(all_Commands)  
+ 
+    command = raw_input("Geben Sie den gewuenschten Befehl ein: ")
+    if command not in all_Commands:
+        print("Fehler! Command nicht vorhanden!")
+    else:
+        #Hier den Command ausfueheren 
+        hostname = raw_input("An welchem Host soll der Command ausgefuehrt werden? (Falls keiner benoetigt wird, einfach mit ENTER bestaetigen) Eingabe: ")
+        t=calendar.timegm(time.gmtime())
+        zeitpunkt = raw_input("Soll dem Command ein bestimmter Zeitpunkt mitgegeben werden? Wenn Nein, dann wird die aktuelle Systemzeit benutzt. Eingabe: ")
+        if zeitpunkt in proceed:
+            print("ES WURDE EIN ANDERER ZEITPUNKT GEWAEHLT!! -> MUSS NOCH GEMACHT WERDEN!")
+        
+        else: 
+            zusatz = raw_input("Werden zusaetzliche Parameter fuer den Command benoetigt? Falls ja, bitte durch Strichpunkt trennen.")
+            try: 
+                #livestatus.SingleSiteConnection(socket_path).do_command("COMMAND %d %s;%s;" % (t,command,hostname))
+                cmdStr = 'lq "COMMAND [$(date +%s)] ' + command + ';' + hostname + ';' + zusatz'"'
+                os.system(cmdStr)
+                
+            
+            except Exception as e:
+                print("Livestatus Error: %s " % str(e))
+    
+
--- prototyp.py
+++ prototyp.py
@@ -3,23 +3,41 @@
 import os, sys, livestatus, time, calendar
 import ChMKbib
 
+abbruch = 0
 
+def menue(menuePunkt):
+    if menuePunkt == 1:
+        ChMKbib.show_Hosts(socket_path)
+    elif menuePunkt == 2:
+        ChMKbib.log_Since_Time(socket_path)
+    elif menuePunkt == 3:
+        ChMKbib.show_Services(socket_path)
+    elif menuePunkt == 4:
+        ChMKbib.show_Availability(socket_path)
+    elif menuePunkt == 5:
+        dataTextfile=ChMKbib.show_New_Logs(socket_path)
+        ChMKbib.write_To_Textfile(dataTextfile)
+    elif menuePunkt == 6:
+        ChMKbib.send_Command(socket_path)
+    return None
 
 socket_path=ChMKbib.local_Connection()
 
-ChMKbib.show_Hosts(socket_path)
+while(abbruch == 0):
+    print("\n\n#################################################\n")
+    print("Bitte einen der folgenden Menuepunkte auswaehlen:")
+    print("1. Hosts anzeigen")
+    print("2. Services anzeigen")
+    print("3. Log anzeigen")
+    print("4. Verfuegbarkeit anzeigen")
+    print("5. Events anzeigen")
+    print("6. Command schicken")
+    menuePunkt = input("Menue-Punkt auswaehlen: ")
+    print("\n#################################################\n\n")
+    menue(menuePunkt)
+    #abbruch = raw_input("Zum Abbrechen eine Taste betaetigen.")
 
-ChMKbib.log_Since_Time(socket_path)
-
-ChMKbib.show_Services(socket_path)
-
-ChMKbib.show_Availability(socket_path)
-
-while(1):    
-    dataTextfile=ChMKbib.show_New_Logs(socket_path)
-    ChMKbib.write_To_Textfile(dataTextfile)
+    
 
 
-#    ChMKbib.show_Logs_Test(socket_path)
-    time.sleep(1)
-#    print ("Test") 
+
