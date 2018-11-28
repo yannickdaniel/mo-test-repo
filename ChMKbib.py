d30d28238f7186ebc80822368814a9df32b869ab
Yannick Daniel <yannick.daniel@siemens.com>
2018-11-28 08:50:16 +0100

    AUF OIS DEV SCHIEBEN

--- ChMKbib.py
+++ ChMKbib.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python2.7
+# -*- encoding: iso-8859-15 -*-
 
 import os, sys, livestatus, time, calendar
 
 
 def log_Since_Time(socket_path):
-    import  sys, livestatus, time, calendar
+    import   livestatus, time, calendar
     t=calendar.timegm(time.gmtime())    
     zeitraum=input("Auf welchen Zeitraum sollen die Ergebnisse eingegrenzt werden? Eingabe in Minuten:")
     zeitraum=zeitraum*60
@@ -18,10 +19,15 @@ def log_Since_Time(socket_path):
         print ("Livestatus Error %s" % str(e))
 
 ##########################################################################################
-def local_Connection():
+def start_Connection():
     try:
-        omd_root = os.getenv("OMD_ROOT")
-        socket_path = "unix:" + omd_root + "/tmp/run/live"
+        conn = raw_input("Lokale oder TCP Verbindung?(local / tcp)\nEingabe:")
+        inp = ["local", "lokal", "Lokal"]
+        if conn in inp:
+            omd_root = os.getenv("OMD_ROOT")
+            socket_path = "unix:" + omd_root + "/tmp/run/live"
+        else:
+            socket_path = "tcp:localhost:6557"
         return socket_path
 
     except:
@@ -82,17 +88,18 @@ def write_To_Textfile(data):
 
 #########################################################################################
 def show_Logs_Test(socket_path):
-    try:
-       services=livestatus.SingleSiteConnection(socket_path).query_table("GET services\nColumns: host_name description state")
-       t = calendar.timegm(time.gmtime())
-       for host_name, description, state in services:
-           
-           logs = livestatus.SingleSiteConnection(socket_path).query_table("GET log\nWaitTrigger: downtime\nWaitTimeout: 5000\nFilter: time >= %d\nColumns: host_name service_description time state" %(t))
-       if logs:
-           print (logs, t)
-           return logs
-    except Exception as e:
-        print ("Livestatus Error %s" % str(e))
+    pass
+   # try:
+    #   services=livestatus.SingleSiteConnection(socket_path).query_table("GET services\nColumns: host_name description state")
+     #  t = calendar.timegm(time.gmtime())
+      # for host_name, description, state in services:
+       #    
+        #   logs = livestatus.SingleSiteConnection(socket_path).query_table("GET log\nWaitTrigger: downtime\nWaitTimeout: 5000\nFilter: time >= %d\nColumns: host_name service_description time state" %(t))
+       #if logs:
+        #   print (logs, t)
+         #  return logs
+    #except Exception as e:
+     #   print ("Livestatus Error %s" % str(e))
 
 #    return logs
 
@@ -102,14 +109,12 @@ def show_Availability(socket_path):
 
     try:
         t=calendar.timegm(time.gmtime())
-        z=input("Geben Sie den Zeitraum ein, in dem die Verfuegbarkeit der Services bestimmt werden soll. (Eingabe in Stunden):")
+        z=input("Geben Sie den Zeitraum ein, in dem die Verfügbarkeit der Services bestimmt werden soll. (Eingabe in Stunden):")
         k=z*3600
         k=t-k
        
         availability=livestatus.SingleSiteConnection(socket_path).query_table("GET statehist\nColumns: host_name service_description\nFilter: time < %d\nFilter: time >= %d\nStats: sum duration_part_ok\nStats: sum duration_part_warning\nStats: sum duration_part_critical\nStats: sum duration_part_unknown\nStats: sum duration_part_unmonitored" %(t,k))
- 
-       
-       
+
         for line in availability:
             print(line)
     except Exception as e:
@@ -118,9 +123,8 @@ def show_Availability(socket_path):
 ###############################################################################
 
 def send_Command(socket_path):
-    import time
-
-    liste_anzeigen = raw_input("Wollen Sie eine Liste mit den verfuegbaren Befehlen angezeigt bekommen?(Ja/Nein) Eingabe: ")
+   
+    liste_anzeigen = raw_input("Wollen Sie eine Liste mit den verfügbaren Befehlen angezeigt bekommen?(Ja/Nein)\nEingabe: ")
     proceed =['y', 'Y', 'Ja', 'ja'] 
     read_obj = open("all_Commands.txt")
     all_Commands = read_obj.read()
@@ -128,26 +132,19 @@ def send_Command(socket_path):
     if liste_anzeigen in proceed: 
        print(all_Commands)  
  
-    command = raw_input("Geben Sie den gewuenschten Befehl ein: ")
+    command = raw_input("Geben Sie den gewünschten Befehl ein: ")
     if command not in all_Commands:
         print("Fehler! Command nicht vorhanden!")
     else:
         #Hier den Command ausfueheren 
-        hostname = raw_input("An welchem Host soll der Command ausgefuehrt werden? (Falls keiner benoetigt wird, einfach mit ENTER bestaetigen) Eingabe: ")
-        t=calendar.timegm(time.gmtime())
-        zeitpunkt = raw_input("Soll dem Command ein bestimmter Zeitpunkt mitgegeben werden? Wenn Nein, dann wird die aktuelle Systemzeit benutzt. Eingabe: ")
-        if zeitpunkt in proceed:
-            print("ES WURDE EIN ANDERER ZEITPUNKT GEWAEHLT!! -> MUSS NOCH GEMACHT WERDEN!")
-        
-        else: 
-            zusatz = raw_input("Werden zusaetzliche Parameter fuer den Command benoetigt? Falls ja, bitte durch Strichpunkt trennen.")
-            try: 
-                #livestatus.SingleSiteConnection(socket_path).do_command("COMMAND %d %s;%s;" % (t,command,hostname))
-                cmdStr = 'lq "COMMAND [$(date +%s)] ' + command + ';' + hostname + ';' + zusatz'"'
-                os.system(cmdStr)
-                
-            
-            except Exception as e:
-                print("Livestatus Error: %s " % str(e))
+        hostname = raw_input("An welchem Host soll der Command ausgeführt werden? \n(Falls nicht, einfach mit ENTER bestätigen) \nEingabe: ")
+        zusatz = raw_input("Werden zusätzliche Parameter fuer den Command benötigt? \n(Falls ja, bitte durch Strichpunkt trennen, nein mit ENTER bestätigen)\nEingabe: ")
+        try: 
+            #livestatus.SingleSiteConnection(socket_path).do_command("COMMAND %d %s;%s;" % (t,command,hostname))
+            cmdStr = 'lq "COMMAND [$(date +%s)] ' + command + ';' + hostname + ';' + zusatz + '"'
+            os.system(cmdStr)
+                    
+        except Exception as e:
+            print("Livestatus Error: %s " % str(e))
     
 
--- prototyp.py
+++ prototyp.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python
+# -*- encoding: iso-8859-15 -*-
+
 
 import os, sys, livestatus, time, calendar
 import ChMKbib
@@ -9,9 +11,9 @@ def menue(menuePunkt):
     if menuePunkt == 1:
         ChMKbib.show_Hosts(socket_path)
     elif menuePunkt == 2:
-        ChMKbib.log_Since_Time(socket_path)
-    elif menuePunkt == 3:
         ChMKbib.show_Services(socket_path)
+    elif menuePunkt == 3:
+        ChMKbib.log_Since_Time(socket_path)
     elif menuePunkt == 4:
         ChMKbib.show_Availability(socket_path)
     elif menuePunkt == 5:
@@ -21,18 +23,18 @@ def menue(menuePunkt):
         ChMKbib.send_Command(socket_path)
     return None
 
-socket_path=ChMKbib.local_Connection()
+socket_path=ChMKbib.start_Connection()
 
 while(abbruch == 0):
     print("\n\n#################################################\n")
-    print("Bitte einen der folgenden Menuepunkte auswaehlen:")
+    print("Bitte einen der folgenden Menü-Punkte auswählen:")
     print("1. Hosts anzeigen")
     print("2. Services anzeigen")
     print("3. Log anzeigen")
-    print("4. Verfuegbarkeit anzeigen")
+    print("4. Verfügbarkeit anzeigen")
     print("5. Events anzeigen")
     print("6. Command schicken")
-    menuePunkt = input("Menue-Punkt auswaehlen: ")
+    menuePunkt = input("Menü-Punkt auswählen: ")
     print("\n#################################################\n\n")
     menue(menuePunkt)
     #abbruch = raw_input("Zum Abbrechen eine Taste betaetigen.")
